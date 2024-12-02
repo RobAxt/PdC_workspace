a6204cb0db2e183c3f304a617e364b050cb974bc
RobAxt <roberto.axt@gmail.com>
2024-12-02 08:26:43 -0300

    Ajustes menores a la configuracion del proyecto

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Entorno de trabajo 72.07 TPE/TPF Protocolo de Comunicaciones
 
-* [TPF 2024 2C - Enunciado](./TPF_2024-2c/TP final 2024 2C.pdf)
+* [TPF 2024 2C - Enunciado](./TPF_2024-2c/'TP final 2024 2C.pdf')
--- TPF_2024-2C/src/main.c
+++ TPF_2024-2C/src/main.c
@@ -2,10 +2,48 @@
  ** @brief Definición de la función principal del programa
  **/
 
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+// Estructura para representar la instancia Singleton
+typedef struct
 {
+  int data;
+} Singleton;
+
+// Variable global para almacenar la instancia
+static Singleton* instance = NULL;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Función para obtener la instancia (thread-safe)
+Singleton* get_instance()
+{
+  pthread_mutex_lock(&mutex);
+  if(instance == NULL)
+  {
+    instance = malloc(sizeof(Singleton));
+    instance->data = 42;
+  }
+  pthread_mutex_unlock(&mutex);
+
+  return instance;
+}
+
+int main(int argc, char* argv[])
+{
+  printf("Compilation Date %s - %s\n", __DATE__, __TIME__);
+
+  Singleton* s1 = get_instance();
+  Singleton* s2 = get_instance();
+
+  // Ambos apuntan a la misma instancia
+  printf("%s:%d |  s1->data = %d, s2->data = %d\n", __FILE__, __LINE__, s1->data, s2->data);
+  printf("%s:%d | *s1 = %p, *s2 = %p\n", __FILE__, __LINE__, (void*)s1, (void*)s2);
+
+  // Liberar la memoria al finalizar
+  free(instance);
+  instance = NULL;
+
   return EXIT_SUCCESS;
 }
